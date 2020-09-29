5e6f9b1c731662a0d2abf959b2a31ece0da7bb8a
venkey <venkeyvenkatesh885@gmail.com>
2020-09-29 21:45:51 +0530

    [venkey]Refactor. added while loop to reach position 100

--- snake_ladder/Program.cs
+++ snake_ladder/Program.cs
@@ -12,29 +12,42 @@ namespace snake_ladder
             Console.WriteLine("You are at position :" + position);
 
             Random random = new Random();
-            
+
+            while (position <= 100)
+            {
                 int dieValue = random.Next(1, 7);
                 Console.WriteLine("Die value is : " + dieValue);
-            int noPlay = 0, ladder = 1, snake = 2;
-            
-            int placeCheck = random.Next(1, 3);
-                if(placeCheck==ladder)
-            {
-                Console.WriteLine("yahoo!! Ladder");
-                position += dieValue;
+                //int noPlay = 0
+                    int ladder = 1, snake = 2;
+
+                int placeCheck = random.Next(1, 3);
+                if (placeCheck == ladder)
+                {
+                    Console.WriteLine("Ladder");
+                    position += dieValue;
+                }
+                else if (placeCheck == snake)
+                {
+                    Console.WriteLine("Snake");
+                    if (position - dieValue < 0)
+                    {
+                        position = 0;
+                    }
+                    else
+                    {
+                        position -= dieValue;
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("No play");
+                    Console.WriteLine("No change in position");
+                    position += 0;
+                }
+
+                Console.WriteLine("The new position after die roll is :" + position);
             }
-                else if(placeCheck==snake)
-            {
-                Console.WriteLine("Oh! no Snake");
-                position -= dieValue;
-            }
-            else
-            {
-                Console.WriteLine("No change in position");
-                position += 0;
-            }
-            
-            Console.WriteLine("The new position after die roll is :" + position);
 
         }
     }
