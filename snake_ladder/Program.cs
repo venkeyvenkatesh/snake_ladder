643612ad3ec8603cf04f756b86cd26009bf83b2e
venkey <venkeyvenkatesh885@gmail.com>
2020-09-29 21:06:50 +0530

    first commit

---
+++ snake_ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace snake_ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to snake ladder game");
+        }
+    }
+}
