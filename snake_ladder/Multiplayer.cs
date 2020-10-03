12ead4ca97c053ebb0707901635a602f32ccca50
venkey <venkeyvenkatesh885@gmail.com>
2020-10-03 12:26:02 +0530

    [venkey]Added Multiplayer class to manage two players at same time

---
+++ snake_ladder/Multiplayer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_ladder
+{
+    class Multiplayer
+    {
+        //variables
+        public int dieRollCount = 0;
+        public static int position1 = 0;
+        public static int position2 = 0;
+
+        /// <summary>
+        /// Positions the after die.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="player">The player.</param>
+        /// <returns></returns>
+        public int PositionAfterDie(int position,int player)
+        {
+
+
+            if (position == 100)
+            {
+                if (player == 1)
+                {
+                    Console.WriteLine("Player 2 is at psotion : " + position2);
+                }
+                Console.WriteLine("Player " + player + " has won");
+
+
+                Environment.Exit(1);
+            }
+            else
+            {
+                int ladder = 1, snake = 2;
+                int placeCheck = getDieRollType();
+                int dieValue = getDieValue();
+                if (placeCheck == ladder)
+                {
+
+
+                    Console.WriteLine("Ladder");
+
+
+                    if (position + dieValue > 100)
+                    {
+                        position = position + 0;
+                    }
+                    else
+                    {
+                        position += dieValue;
+                    }
+
+                    Console.WriteLine("The player" + player + "  is at  :" + position);
+                    position = PositionAfterDie(position, player);
+
+                }
+                else if (placeCheck == snake)
+                {
+                    Console.WriteLine("Snake");
+                    if (position - dieValue < 0)
+                    {
+                        position = 0;
+                    }
+                    else
+                    {
+                        position -= dieValue;
+                    }
+                    Console.WriteLine("The player" + player + "  is at  :" + position);
+
+                }
+                else
+                {
+                    Console.WriteLine("No play");
+
+                    position += 0;
+                    Console.WriteLine("The player" + player + "  is at  :" + position);
+                }
+
+
+               
+            }
+            return position;
+        }
+
+
+        /// <summary>
+        /// Gets the die value.
+        /// </summary>
+        /// <returns></returns>
+        public int getDieValue()
+        {
+            Random random = new Random();
+            int dieValue = random.Next(1, 7);
+            Console.WriteLine("Die value is : " + dieValue);
+            return dieValue;
+        }
+
+        /// <summary>
+        /// Gets the type of the die roll.
+        /// </summary>
+        /// <returns></returns>
+        public int getDieRollType()
+        {
+            Random random = new Random();
+           
+            int placeCheck = random.Next(1, 4);
+            return placeCheck;
+        }
+
+
+
+        /// <summary>
+        /// Starts the ladder game.
+        /// </summary>
+        public void SnakeLadderGame()
+        {
+            
+            Console.WriteLine("Player 1 is at position :" + position1);
+            Console.WriteLine("Player 2 is at position :" + position2);
+
+           
+            int player1 = 1;
+            int player2 = 2;
+            while (position1 < 100 && position2 < 100)
+            {
+
+                dieRollCount++;
+                
+                Console.WriteLine("Die roll count : " + dieRollCount);
+               
+                position1 = PositionAfterDie(position1,player1);
+                position2 = PositionAfterDie(position2,player2);
+
+              
+                Console.WriteLine("\n");
+            }
+
+           
+        }
+
+      
+    }
+}
+
--- snake_ladder/Program.cs
+++ snake_ladder/Program.cs
@@ -5,110 +5,27 @@ namespace snake_ladder
     class Program
     {
 
-        public  int dieRollCount=0;
+
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to snake ladder game");
-            // Console.WriteLine(SnakeLadderGame());
-
-            Program player1 = new Program();
-              Program player2 = new Program();
-
-            //  snake_ladder player2 = new snake_ladder();
-
-            player1.SnakeLadderGame();
-            player2.SnakeLadderGame();
-            Console.WriteLine("The die roll count for player 1 is :" + player1.dieRollCount);
-            Console.WriteLine("The die roll count for player 2 is :" + player2.dieRollCount);
-
-            if (player1.dieRollCount > player2.dieRollCount)
-            {
-                Console.WriteLine("Player 2 won");
-            }
-            else
-            {
-                Console.WriteLine("Player 1 won");
-            }
-
-        }
-
-        public  int PositionAfterDie(int position, int dieValue)
-        {
-            //dieRollCount++;
-
-            Random random = new Random();
-            int ladder = 1, snake = 2;
-            int placeCheck = random.Next(1, 4);
-            if (placeCheck == ladder)
-            {
-               
-
-                Console.WriteLine("Ladder");
-                
-                    if (position + dieValue > 100)
-                    {
-                        position = position + 0;
-                    }
-                    else
-                    {
-                        position += dieValue;
-                    }
-                
-             // position= PositionAfterDie(position, getDieValue());
-                //Console.WriteLine("The new position after die roll is :" + position);
-            }
-            else if (placeCheck == snake)
-            {
-                Console.WriteLine("Snake");
-                if (position - dieValue < 0)
-                {
-                    position = 0;
-                }
-                else
-                {
-                    position -= dieValue;
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("No play");
-
-                position += 0;
-            }
-           // Console.WriteLine("The new position after die roll is :" + position);
-            return position;
-        }
-        public int getDieValue()
-        {
-            Random random = new Random();
-            int dieValue = random.Next(1, 7);
-            Console.WriteLine("Die value is : " + dieValue);
-            return dieValue;
-        }
-        public  void  SnakeLadderGame()
-        {
-            int position = 0;
-          // int dieRollCount = 0;
-            Console.WriteLine("You are at position :" + position);
             
-            int dieValue = 0;
-            int newPosition = 0;
-            while (newPosition < 100)
-            {
 
-                dieRollCount++;
-                dieValue = getDieValue();
+            Multiplayer play = new Multiplayer();
+          
 
-                Console.WriteLine("Die count :" + dieRollCount);
-                newPosition = PositionAfterDie(newPosition, dieValue);
-
-                Console.WriteLine("The new position after die roll is :" + newPosition);
-               
-            }
+            play.SnakeLadderGame();
            
-            //return dieRollCount;
+
         }
-        }
+
+
+
     }
+}
 
